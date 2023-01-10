b799ec1db7aec4ba548e3fc8c2a2889b9cf6cf6c
Libertad Pozos <libertad.pozos@gmail.com>
2023-01-10 15:40:26 +0000

    Add create-reaact-app template with lintern and prettier

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <p>HELLO WORLD</p>
+    </div>
+  );
+}
+
+export default App;
